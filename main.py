540bbb188bd69ee7db050e7e5746e7c720c9cd94
lyangelaa <160497319+lyangelaa@users.noreply.github.com>
2024-03-09 14:19:17 +0000

    final submission

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ stdform = input('Enter a number in scientific notation: ')
 stdform = stdform.strip()
 
 # Type your code below
+enotation = stdform.replace('x10^', 'E')
+print('This number in E notation is ' + enotation + '.')
